8ba389aec2907bad4da9a20cdab2d459b72ad0bb
unknown <abhishekyesankar12@gmail.com>
2022-12-29 21:57:52 +0530

    minor changes

--- assets/js/advance-slider.js
+++ assets/js/advance-slider.js
@@ -1,179 +1,165 @@
 class advanceSlider extends elementorModules.frontend.handlers.Base {
-  // constructor( elements ) {
-  //  	super( elements );
-  //  	this.newSwiper( this.elements.$container );
-  //  	// console.log( this.getElementSettings( 'slide_per_group' ) );
-  // }
-
-  getDefaultSettings() {
-    return {
-      selectors: {
-        container: ".quantum-swiper-container",
-      },
-    };
-  }
-
-  getDefaultElements() {
-    const selectors = this.getSettings("selectors");
-
-    return {
-      $container: this.$element.find(selectors.container),
-    };
-  }
-
-  onInit() {
-    console.log(this.getDefaultElements().$container);
-  }
-
-  getSwiperDefaultConfig() {
-    const settings = this.getElementSettings;
-
-    return {
-      direction: "horizontal",
-      loop: settings("loop") ? true : false,
-      pagination: {
-        el: ".swiper-pagination",
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      scrollbar: {
-        el: ".swiper-scrollbar",
-      },
-    };
-  }
-
-  /**
-   * add responsive settings
-   * @param Object of swiper config
-   * @param Object with key of the swiper option and the value of elementId
-   * @returns Object with breakpoints
-   */
-  addBreakPointSettings(config, args) {
-    const settings = this.getElementSettings,
-      allBreakPoints = elementorFrontend.config.responsive.activeBreakpoints,
-      breakpointName = Object.keys(allBreakPoints);
-    let breakpointSettings = {},
-      lastBreakpoint;
-
-    args.forEach((option) => {
-      lastBreakpoint = null;
-      const arg = Object.keys(option);
-      const optionKey = arg[0];
-      const optionValue = option[optionKey];
-      const optionType = option.type;
-
-      for (let i = 0; i <= breakpointName.length; i++) {
-        let breakpointBreakpoint, elementValue;
-
-        if (i !== breakpointName.length) {
-          elementValue = settings(optionValue + "_" + breakpointName[i]);
-        } else if (i === breakpointName.length)
-          elementValue = settings(optionValue);
-
-        if (elementValue === undefined || elementValue === "") continue;
-
-        if (optionType !== undefined) {
-          if (optionType === "BOOLEAN") {
-            if (elementValue == 0) {
-              elementValue = false;
-            } else elementValue = true;
-          } else if (optionType === "NUMBER") {
-            if (parseInt(elementValue) === NaN) {
-              console.error(optionValue + " Needs to be a number");
-              continue;
-            }
-          }
-        } else {
-          console.error("You have to define type of " + optionValue);
-          continue;
-        }
-
-        if (i === breakpointName.length) {
-          breakpointBreakpoint = {
-            [optionKey]: elementValue,
-          };
-
-          breakpointSettings[lastBreakpoint] = {
-            ...breakpointSettings[lastBreakpoint],
-            ...breakpointBreakpoint,
-          };
-        } else if (i < breakpointName.length) {
-          if (i === 0) {
-            config[optionKey] = elementValue;
-          } else if (i > 0) {
-            breakpointBreakpoint = {
-              [optionKey]: elementValue,
-            };
-
-            breakpointSettings[lastBreakpoint] = {
-              ...breakpointSettings[lastBreakpoint],
-              ...breakpointBreakpoint,
-            };
-          }
-
-          lastBreakpoint = allBreakPoints[breakpointName[i]].value;
-          if (allBreakPoints[breakpointName[i]].direction === "max") {
-            lastBreakpoint++;
-          }
-        }
+   getDefaultSettings() {
+      return {
+         selectors: {
+            container: ".quantum-swiper-container",
+         },
       }
-    });
+   }
 
-    config.breakpoints = {...breakpointSettings};
-    return config;
-  }
+   getDefaultElements() {
+      const selectors = this.getSettings( "selectors" );
 
-  newSwiper(el) {
-    const responsiveOptions = [
-      {
-        slidesPerView: "slide_per_view",
-        type: "NUMBER",
-      },
-      {
-        centeredSlides: "center_slide",
-        type: "BOOLEAN",
-      },
-      {
-        spaceBetween: "space_between",
-        type: "NUMBER",
-      },
-      {
-        slidesPerGroup: "slide_per_group",
-        type: "NUMBER",
-      },
-    ];
+      return {
+         $container: this.$element.find( selectors.container ),
+      }
+   }
 
-    let defaultSwiperConfig = this.getSwiperDefaultConfig(),
-      swiperConfig = this.addBreakPointSettings(
-        defaultSwiperConfig,
-        responsiveOptions
-      );
-    console.log(swiperConfig);
+   onInit() {
+      this.newSwiper( this.getDefaultElements().$container );
+   }
 
-    // Not using typeof Swiper condition logic here because
-    // if you link swiper lib via CDN element built with elementor's
-    // swiper instance won't work
-    //
-    // P.S. I have not put lots of thought into it and maybe change it later
-    //
-    const Swiper = elementorFrontend.utils.swiper;
-    new Swiper(el, swiperConfig).then((newSwiperInstance) => {
-      let mySwiper = newSwiperInstance;
-      this.elements.$container.data("swiper", mySwiper);
-      // console.log( this.elements.$container.data() );
-    });
-  }
+   newSwiper( el ) {
+      const responsiveOptions = [
+         {
+            slidesPerView: "slide_per_view",
+            type: "NUMBER",
+         },
+         {
+            centeredSlides: "center_slide",
+            type: "BOOLEAN",
+         },
+         {
+            spaceBetween: "space_between",
+            type: "NUMBER",
+         },
+         {
+            slidesPerGroup: "slide_per_group",
+            type: "NUMBER",
+         },
+      ];
+
+      let defaultSwiperConfig = this.getSwiperDefaultConfig(),
+         swiperConfig = this.addBreakPointSettings(
+            defaultSwiperConfig,
+            responsiveOptions
+         );
+      console.log( swiperConfig );
+
+      /// Not using typeof Swiper condition logic here because
+      /// if you link swiper library via any other source the
+      /// element built with elementor's swiper instance won't work
+    
+      const Swiper = elementorFrontend.utils.swiper;
+      new Swiper( el, swiperConfig ).then( ( newSwiperInstance ) => {
+         let mySwiper = newSwiperInstance;
+         this.getDefaultElements().$container.data( "swiper", mySwiper );
+         // console.log( this.elements.$container.data() );
+      });
+   }
+
+   getSwiperDefaultConfig() {
+      const settings = this.getElementSettings;
+
+      return {
+         direction: "horizontal",
+         loop: settings( "loop" ) ? true : false,
+         pagination: {
+            el: ".swiper-pagination",
+         },
+         navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+         },
+         scrollbar: {
+            el: ".swiper-scrollbar",
+         },
+      }
+   }
+
+   addBreakPointSettings( config, args ) {
+      const settings = this.getElementSettings,
+         allBreakPoints = elementorFrontend.config.responsive.activeBreakpoints,
+         breakpointName = Object.keys( allBreakPoints );
+      let breakpointSettings = {},
+         lastBreakpoint;
+
+      args.forEach( ( option ) => {
+         lastBreakpoint = null;
+         const arg = Object.keys( option );
+         const optionKey = arg[0];
+         const optionValue = option[optionKey];
+         const optionType = option.type;
+
+         for( let i = 0; i <= breakpointName.length; i++ ) {
+            let breakpointBreakpoint, elementValue;
+
+            if( i !== breakpointName.length ) {
+               elementValue = settings( optionValue + "_" + breakpointName[i] );
+            } else if( i === breakpointName.length )
+               elementValue = settings( optionValue );
+
+            if( elementValue === undefined || elementValue === "" ) continue;
+
+            if( optionType !== undefined ) {
+               if( optionType === "BOOLEAN" ) {
+                  if( elementValue == 0 ) {
+                     elementValue = false;
+                  } else elementValue = true;
+               } else if( optionType === "NUMBER" ) {
+                  if( parseInt( elementValue ) === NaN ) {
+                     console.error( optionValue + " Needs to be a number" );
+                     continue;
+                  }
+               }
+            } else {
+               console.error( "You have to define type of " + optionValue );
+               continue;
+            }
+
+            if( i === breakpointName.length ) {
+               breakpointBreakpoint = {
+                  [optionKey]: elementValue,
+               }
+
+               breakpointSettings[lastBreakpoint] = {
+                  ...breakpointSettings[lastBreakpoint],
+                  ...breakpointBreakpoint,
+               }
+            } else if( i < breakpointName.length ) {
+               if( i === 0 ) {
+                  config[ optionKey ] = elementValue;
+               } else if( i > 0 ) {
+                  breakpointBreakpoint = {
+                     [optionKey]: elementValue,
+                  }
+
+                  breakpointSettings[lastBreakpoint] = {
+                     ...breakpointSettings[lastBreakpoint],
+                     ...breakpointBreakpoint,
+                  }
+               }
+
+               lastBreakpoint = allBreakPoints[breakpointName[i]].value;
+               if( allBreakPoints[ breakpointName[i]].direction === "max" ) {
+                  lastBreakpoint++;
+               }
+            }
+         }
+      });
+
+      config.breakpoints = { ...breakpointSettings };
+      return config;
+   }
 }
 
-jQuery(window).on("elementor/frontend/init", () => {
-  const addHandler = ($element) => {
-    elementorFrontend.elementsHandler.addHandler(advanceSlider, {$element});
-  };
+jQuery( window ).on( "elementor/frontend/init", () => {
+   const addHandler = ( $element ) => {
+      elementorFrontend.elementsHandler.addHandler(advanceSlider, { $element });
+   };
 
-  elementorFrontend.hooks.addAction(
-    "frontend/element_ready/Advance_slider.default",
-    addHandler
-  );
+   elementorFrontend.hooks.addAction(
+      "frontend/element_ready/Advance_slider.default",
+      addHandler
+   );
 });
